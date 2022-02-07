9cbf76eafee8ad02cd15f0fadb220baccba06a6c
lcw729 <lcw729729@gmail.com>
2022-02-07 11:52:25 +0900

    kubeadm allow-downgrades

--- 3.Upgrade_kubeadm.sh
+++ 3.Upgrade_kubeadm.sh
@@ -1,5 +1,5 @@
 apt-mark unhold kubeadm && \
-apt-get update && apt-get install -y kubeadm=$1 && \
+apt-get update && apt-get install -y kubeadm=$1 --allow-downgrades && \
 apt-mark hold kubeadm
 
 kubeadm version
