c9db5ca7d709da3c72f91ec4159e5349c59aa203
lcw729 <lcw729729@gmail.com>
2022-02-10 14:18:14 +0900

    add uninstall docker & kubernetes shell script

---
+++ 3.upgrade_kubeadm.sh
@@ -0,0 +1,7 @@
+apt-mark unhold kubeadm && \
+apt-get update && apt-get install -y kubeadm=$1 --allow-downgrades && \
+apt-mark hold kubeadm
+
+kubeadm version
+kubeadm upgrade plan
+
--- 4.Upgrade_kubectl_client.sh
+++
@@ -1,6 +0,0 @@
-curl -LO https://dl.k8s.io/release/v$1/bin/linux/amd64/kubectl
-curl -LO https://dl.k8s.io/v$1/bin/linux/amd64/kubectl.sha256
-echo "$(<kubectl.sha256) kubectl" | sha256sum --check
-
-chmod +x ./kubectl
-sudo mv ./kubectl $(which kubectl)
